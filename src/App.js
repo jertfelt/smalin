b01752529d9168ced29af0f444d81af877acc2ba
Tova Jertfelt <tovizitan@gmail.com>
2022-08-30 13:17:28 +0200

    lagt in routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Startpage from "./views/Startpage";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    
+  <Router>
     <div className="App">
-     
+     <Routes>
+      <Route exact path="/" element={<Startpage />}></Route>
+     </Routes>
     </div>
+  </Router>
   );
 }
 
---
+++ src/views/Startpage.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Startpage = () => {
+  return ( 
+  <div className="welcome">
+    <h1 className="really-big-text">Välkomna!</h1>
+    <div>
+
+    </div>
+  </div> );
+}
+ 
+export default Startpage;
